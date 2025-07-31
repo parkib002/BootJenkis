8a18ee94cceb11a30e12b17cda0888f9561a0a35
parkib002 <parkib003@naver.com>
2025-07-31 10:42:31 +0900

    jenkins

---
+++ src/main/java/spring/boot/jenkins/JenkinsBootProjectApplication.java
@@ -0,0 +1,13 @@
+package spring.boot.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsBootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsBootProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/boot/jenkins/JenkinsBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.boot.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsBootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
