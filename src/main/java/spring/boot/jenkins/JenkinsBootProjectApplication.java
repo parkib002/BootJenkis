b73ad6b293fcbc7b9f1bae10a7b813605862e879
parkib002 <parkib003@naver.com>
2025-07-31 11:27:24 +0900

    jenkins

--- src/main/java/spring/boot/jenkins/JenkinsBootProjectApplication.java
+++ src/main/java/spring/boot/jenkins/JenkinsBootProjectApplication.java
@@ -2,8 +2,10 @@ package spring.boot.jenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("spring.boot.*")
 public class JenkinsBootProjectApplication {
 
 	public static void main(String[] args) {
